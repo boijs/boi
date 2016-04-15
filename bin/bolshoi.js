67b5f6b84f9667dfa3521673712f8917fe87e047
zhoujunpeng <zjp0432@163.com>
2016-04-15 17:03:12 +0800

    初步走通build行为

--- bin/bolshoi.js
+++ bin/bolshoi.js
@@ -2,11 +2,15 @@
 'use strict'
 
 let program = require('commander');
-let fsp = require('fs-promise');
+let Promise = require("bluebird");
+let fs = require("fs");
 let path = require('path');
 // let promptly = require('promptly');
 
-let bolshoi = require('bolshoi-kernel');
+// let bolshoi = require('bolshoi-kernel');
+
+// local debug 
+let bolshoi = require('../../bolshoi-kernel');
 
 program.version('0.0.1');
 
@@ -21,17 +25,16 @@ program.command('init [dir]')
 program.command('build')
     .description('build project files')
     .action(function() {
-        let _currentDir = path.resolve(process.cwd(),'./');
+        let _confFile = path.join(process.cwd(),'/bolshoi-conf.js');
         
-        let _conf = require(path.join(_currentDir,'/bolshoi-conf.js'));
-        bolshoi.config(_conf);
-        bolshoi.config.generateConfFile();
-        
-        // fsp.readFile(path.join(_currentDir,'/bolshoi-conf.js'))
-        // .then(function(conf){
-        //     console.log(conf);
-        // }).catch(function(){
-        //     console.log('Read configuration fils failed!');
-        // });
+        Promise.try(function(){
+            return fs.statSync(_confFile);
+        }).then(function(stat){
+            require(_confFile);
+        }).then(function(){
+            bolshoi.config.generateConfFile();
+        }).catch(function(err){
+            console.log(err);
+        });
     });
 program.parse(process.argv);
