d7f15df0e373e4b03a3c003a98dd10bb4d0ba54c
zhoujunpeng <zjp0432@163.com>
2016-05-13 10:55:45 +0800

    加入file watch

--- README.md
+++ README.md
@@ -51,6 +51,12 @@ boi.use('boi-plugin-loader-vue');
 
 boi会判断用户是否已安装此插件，如果没有，则boi会自动安装此插件。
 
-如果npm被墙，请尝试以下*任意一种*方案： 1. 挂VPN； 2. 修改npm仓库到淘宝镜像`npm config set registry https://registry.npm.taobao.org`; 3. 安装cnpm。
+> 建议自行安装插件，boi使用npm安装插件，由于一些*原因*可能会安装失败
+
+如果npm被墙，请尝试以下*任意一种方案：
+
+1.	挂VPN；
+2.	修改npm仓库到淘宝镜像`npm config set registry https://registry.npm.taobao.org`;
+3.	安装cnpm。
 
 > 如果安装cnpm，请务必自行安装插件
--- bin/boi.js
+++ bin/boi.js
@@ -13,44 +13,105 @@ let ora = require('ora');
 let colors = require('colors');
 
 require('shelljs/global');
-// 引入boli-kernel模块
-require('boli-kernel');
-
+// 引入boi-kernel模块
+// let boi = require('boi-kernel');
 // local debug
-// require('../../boli-kernel');
+let boi = require('../../boi-kernel');
+
+//将boi暴露为全局变量
+Object.defineProperty(global, 'boi', {
+    enumerable: true,
+    writable: false,
+    value: boi
+});
 
 let spinner = null;
+let confChanged = true;
+
+let watchConfFile = function(_confFile) {
+    fs.watch(_confFile, function(event, filename) {
+        if (event === 'change') {
+            console.log(colors.blue('Configuration changed'));
+            confChanged = true;
+        }
+    })
+};
 
 program.version('0.0.1');
 
 program.command('init [dir]')
-    .description('download waterloo analytics sourecode and init')
+    .description('init')
     .action(function(dir) {
         if (!dir || dir === '/') {
             dir = './';
         }
     });
 
-program.command('build')
+program.command('build [env]')
     .description('build project files')
-    .action(function() {
+    .action(function(env) {
         spinner = ora(colors.blue('Building...\n'));
         spinner.start();
-        let _confFile = path.join(process.cwd(), '/boli-conf.js');
+        let _confFile = path.join(process.cwd(), '/boi-conf.js');
 
-        Promise.try(function() {
-            return fs.statSync(_confFile);
-        }).then(function(stat) {
-            require(_confFile);
-        }).then(function() {
-            boli.resolvePlugins();
-        }).then(function() {
-            boli.runBuild();
-        }).then(function() {
-            spinner.stop();
-        }).catch(function(err) {
-            spinner.stop();
-            throw new Error(err);
-        });
+        if (confChanged) {
+            Promise.try(function() {
+                return fs.statSync(_confFile);
+            }).then(function(stat) {
+                require(_confFile);
+                confChanged = false;
+                // 首次执行配置行为之后监听配置文件
+                // 如果没有改动则不必每次build都执行配置行为，以节省build时间
+                watchConfFile(_confFile);
+            }).then(function() {
+                boi.resolvePlugins();
+            }).then(function() {
+                boi.runBuild(env);
+            }).then(function() {
+                spinner.stop();
+            }).catch(function(err) {
+                spinner.stop();
+                throw new Error(err);
+            });
+        } else {
+            Promise.try(function() {
+                boi.runBuild(env);
+            }).then(function() {
+                spinner.stop();
+            }).catch(function(err) {
+                spinner.stop();
+                throw new Error(err);
+            });
+        }
+
     });
+
+program.command('run server')
+    .description('run dev server')
+    .action(function() {
+        let _confFile = path.join(process.cwd(), '/boi-conf.js');
+        if (confChanged) {
+            Promise.try(function() {
+                return fs.statSync(_confFile);
+            }).then(function(stat) {
+                require(_confFile);
+                confChanged = false;
+                watchConfFile(_confFile);
+            }).then(function() {
+                boi.resolvePlugins();
+            }).then(function() {
+                boi.runDevServer(env);
+            }).catch(function(err) {
+                throw new Error(err);
+            });
+        } else {
+            Promise.try(function() {
+                boi.runDevServer(env);
+            }).catch(function(err) {
+                throw new Error(err);
+            });
+        }
+
+    });
+
 program.parse(process.argv);
